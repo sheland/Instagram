8ae3af0d0f0f186183ae5a8b2fc1a87a229af804
Shelan <shelandebesai@gmail.com>
2019-02-22 01:47:35 -0800

    added comment section

--- app/screens/upload.js
+++ app/screens/upload.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Flatlist, StyleSheet, Text, View, Image } from 'react-native';
+import { TextInput, ActivityIndicator, TouchableOpacity, Flatlist, StyleSheet, Text, View, Image } from 'react-native';
 import { f, auth, database, storage } from '../../config/config.js';
 import { Permissions, ImagePicker } from 'expo';
 
@@ -9,7 +9,10 @@ class upload extends React.Component{
     super(props);
     this.state = {
       loggedin: false,
-      imageId: this.uniqueId()
+      imageId: this.uniqueId(),
+      imageSelected: false,
+      uploading: false,
+      caption: ''
     }
   }
 
@@ -32,10 +35,10 @@ class upload extends React.Component{
 
   //return sequence of s4()
   uniqueId = () => {
-    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-'+
-    this.s4() + '-'+ this.s4() + '-'+ this.s4() + '-'+ this.s4()
-
-  }
+    return (
+      this.s4() + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4() + "-" + this.s4()
+    );
+  };
 
   //ask for permission
   //call to select image
@@ -53,21 +56,28 @@ class upload extends React.Component{
     this._checkPermissions();
 
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: 'Images',
+      mediaTypes: "Images",
       allowsEditing: true,
       quality: 1
     });
 
     console.log(result);
 
-    if(!result.cancelled){
-      console.log('upload pic');
-      this.uploadImage(result.uri)
-
-    }else{
-      console.log('cancel');
+    if (!result.cancelled) {
+      console.log("upload image");
+      this.setState({
+        imageSelected: true,
+        imageId: this.uniqueId(),
+        uri: result.uri
+      });
+      //this.uploadImage(result.uri);
+    } else {
+      console.log("cancel");
+      this.setState({
+        imageSelected: false
+      });
     }
-  }
+  };
 
   //check extension type of image
   //re-> looks for last "." and pass the word after the '.' ex: "filename.png"
@@ -87,11 +97,20 @@ class upload extends React.Component{
     var ext = re.exec(uri)[1];
     this.setState({currentFileType: ext});
 
-    const response = await fetch(uri);
-    const blob = await response.blob();
+    // const response = await fetch(uri);
+    // const blob = await response.blob();
     var FilePath = imageId+'.'+that.state.currentFileType;
 
-    const ref = storage.ref('user/'+userid+'/img').child(FilePath);
+    const oReq = new XMLHttpRequest();
+    oReq.open("GET", uri, true);
+    oReq.responseType = "blob";
+    oReq.onload = () => {
+      const blob = oReq.response;
+      //Call function to complete upload with the new blob to handle the uploadTask
+      this.completeUploadBlob(blob, FilePath);
+    };
+    oReq.send();
+    // const ref = storage.ref('user/'+userid+'/img').child(FilePath);
 
     var snapshot = ref.put(blob).on('state_changed', snapshot => {
       console.log('Progress!', snapshot.bytesTransferred, snapshot.totalBytes)
@@ -122,13 +141,36 @@ class upload extends React.Component{
       <View style={{flex: 1}}>
         { this.state.loggedin == true ? (
           //logged in
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text style={{ fontSize: 28, paddingBottom: 15}}>Upload</Text>
-          <TouchableOpacity
-          onPress={ () => this.findNewImage()}
-          style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}>
-          <Text style={{color: 'white'}}>Select Photo</Text>
-          </TouchableOpacity>
+          <View style={{flex: 1}}>
+          { this.state.imageSelected == true ? (
+            <View style={{flex: 1}}>
+              <View style={{height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: '#D0D0D0', borderBottomWidth: 0.5, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Upload</Text>
+              </View>
+              <View style={{padding: 5}}>
+                <Text style={{marginTop: 5}}>Caption:</Text>
+                <TextInput
+                  editable={true}
+                  placeholder={'Enter your caption..'}
+                  maxLength={150}
+                  multiline={true}
+                  numberofLife={4}
+                  onChangeText={(text) => this.setState({caption: text})}
+                  style={{marginVertical: 10, height: 100, padding: 5, borderColor: 'grey', borderWidth: 1, borderRadius: 3, backgroundColor: 'white', color: 'black'}}
+                />
+              </View>
+          </View>
+          ) : (
+
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={{ fontSize: 28, paddingBottom: 15}}>Upload</Text>
+            <TouchableOpacity
+            onPress={ () => this.findNewImage()}
+            style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}>
+            <Text style={{color: 'white'}}>Select Photo</Text>
+            </TouchableOpacity>
+            </View>
+            )}
           </View>
         ) : (
           //not logged in
